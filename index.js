245c431620b2166dc655b4a79a9950bef9bb9d23
IrynaKrt <rinakashi13@mail.ru>
2023-05-03 17:40:03 +0300

    fix: debug the delete

--- index.js
+++ index.js
@@ -613,7 +613,7 @@ class Keyboard {
 
     this.attention = document.createElement('div');
     this.attention.className = 'attention';
-    this.attention.innerText = 'Задание выполнено на базе Windows \n Для переключения между языками можно использовать меню или сочетание Alt + Shift';
+    this.attention.innerText = 'Задание выполнено на базе Windows \n Для переключения между языками можно использовать меню или сочетание Alt + Shift(одновременно)';
     this.main.appendChild(this.attention);
 
     //Keys
@@ -1093,7 +1093,7 @@ class Keyboard {
 
     keys.forEach(key => {
       key.addEventListener('click', (e) => {
-        let newChar = ''
+        let newChar = '';
         //Input
         if (key.classList.contains('caps-lock-key')) {
           newChar += '';
@@ -1105,6 +1105,8 @@ class Keyboard {
           newChar += ' ';
         } else if (key.getAttribute('keyname') === 'Alt') {
           this.textArea.blur();
+        }  else if (key.getAttribute('keyname') === 'Ctrl') {
+          newChar += '';
         } else if (key.getAttribute('data') === 'Backspace') {
           this.select(key);
         } else if (key.getAttribute('data') === 'Delete') {
@@ -1127,6 +1129,9 @@ class Keyboard {
           if (key.getAttribute('data') === 'Tab') {
             this.textArea.selectionStart = start + 3;
             this.textArea.selectionEnd = start + 3;
+          } else if (key.getAttribute('data') === 'Delete' || key.getAttribute('data') === 'Backspace') {
+            this.textArea.selectionStart = start === 0 ? 0 : start;
+            this.textArea.selectionEnd = start === 0 ? 0 : start;
           } else {
             this.textArea.selectionStart = start + 1;
             this.textArea.selectionEnd = start + 1;
